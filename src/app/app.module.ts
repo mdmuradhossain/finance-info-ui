a8aeceb81581e9e6cbc00d71f0be4e61ba18c88c
Md Murad Hossain <mdmuradhossain33@gmail.com>
2022-01-18 13:18:48 +0600

    exchange-rate component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ExchangeRateComponent } from './exchange-rate/exchange-rate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExchangeRateComponent
   ],
   imports: [
     BrowserModule
